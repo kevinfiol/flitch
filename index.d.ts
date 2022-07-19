9f5d04d6c72191c0b697c217e60a565d5cfa66e2
kevinfiol <me@kevinfiol.com>
2022-07-19 01:28:00 -0400

    update typedefs + npmignore

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-type Callback = () => void | Promise<void>;
+type Callback = () => any;
 
 type TestFn =
     ((label: string, testcase: Callback, cleanup?: Callback) => void | Promise<void>) |
@@ -6,14 +6,16 @@ type TestFn =
     ((label: string, testcase: Callback, timeout?: number, cleanup?: Callback) => void | Promise<void>) |
     ((label: string, testcase: Callback, timeout?: number) => void | Promise<void>);
 
+type SuiteFn = (name: string, opts: { timeout: number }) => Suite;
+
 type Suite = TestFn & {
     test: TestFn;
     only: TestFn;
     not: (label: string) => void;
     skip: (label: string) => void;
-    run: () => Promise<void>;
     before: { each: Callback, all: Callback };
     after: { each: Callback, all: Callback };
 };
 
-export function suite(name: string, options: { timeout: number }): Suite;
+export const suite: SuiteFn & { not: SuiteFn, skip: SuiteFn, only: SuiteFn };
+export function run(opts: { parallel: boolean }): Promise<void>;
