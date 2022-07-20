8b148df1932e1f5a37370a070b87286d73b2a253
kevinfiol <me@kevinfiol.com>
2022-07-19 23:18:09 -0400

    0.6.4

--- index.js
+++ index.js
@@ -44,10 +44,9 @@ function printSuite({ name, errors, passes, failures, skip, time }) {
 }
 
 export function run({ parallel = false } = {}) {
-    for (let name in SUITES) {
+    for (let name in SUITES)
         if (!skipSuite.includes(name) && !(onlySuite && name != onlySuite))
             QUEUE.push(SUITES[name]);
-    }
 
     let end = start();
 
@@ -61,20 +60,16 @@ export function run({ parallel = false } = {}) {
     ).then(results =>
         results.map(printSuite)
     ).finally(_ => {
-        p(`• • •\n\nPassed:  ${totalPasses}\nFailed:  ${totalFailures}\nSkipped: ${totalSkips}`);
-        if (skipSuite.length || onlySuite) {
-            p();
+        p(`• • •\n\nPassed:  ${totalPasses}\nFailed:  ${totalFailures}\nSkipped: ${totalSkips}\n`);
+        if (skipSuite.length || onlySuite)
             p(`↷ ${skipSuite.length || Object.keys(SUITES).length - 1} suites skipped.`, 30, 43);
-            p();
-        }
-
-        p(`Duration: ${end()}\n`);
+        p(`\nDuration: ${end()}\n`);
         process.exit(fail ? 1 : 0);
     });
 }
 
-suite.not = suite.skip = (name, ...args) =>
-    skipSuite.push(name) && suite(name, ...args);
+suite.not = suite.skip = name =>
+    skipSuite.push(name) && suite(name);
 
 suite.only = (name, ...args) =>
     (onlySuite = name || 1) && suite(name, ...args);
@@ -93,19 +88,14 @@ export function suite(name, { timeout = 1 } = {}) {
     $.test = $;
     $.before = { each: noop, all: noop };
     $.after = { each: noop, all: noop };
-
+    $.not = $.skip = _ => skip++;
     $.only = (label, testcase, x, y) =>
         onlyTest = [label, testcase, x, y];
-
-    $.not = $.skip = _ => skip++;
 
     SUITES[name] = _ => chain().then(async _ => {
         let end = start();
         await runOp('before.all hook', chain(ctx, $.before.all));
-
-        if (onlyTest)
-            skip += tests.length,
-            tests = [onlyTest];
+        if (onlyTest) skip += tests.length, tests = [onlyTest];
 
         for (let [label, testcase, x, y] of tests)
             await runOp(
@@ -119,13 +109,8 @@ export function suite(name, { timeout = 1 } = {}) {
             );
 
         await runOp('after.all hook', chain(ctx, $.after.all));
-
-        if (failures)
-            totalFailures += failures,
-            fail = true;
-
+        if (failures) totalFailures += failures, fail = true;
         if (skip) totalSkips += skip;
-
         return { name, errors, passes, failures, skip, time: end() };
     });
 
@@ -134,17 +119,9 @@ export function suite(name, { timeout = 1 } = {}) {
         let cleanup = isFn(x) ? x : (isFn(y) ? y : noop);
         let addError = e => errors.push([label, e.message || e]);
 
-        return race(label, op, opTimeout)
-            .then(onSuccess)
+        return race(label, op, opTimeout).then(onSuccess)
             .catch(e => addError(e) && onFail())
-            .finally(_ =>
-                race(
-                    label,
-                    chain(ctx, cleanup, onComplete),
-                    timeout
-                )
-                .catch(addError)
-            );
+            .finally(_ => race(label, chain(ctx, cleanup, onComplete), timeout).catch(addError));
     }
 
     return $;
